4c574fbbf1efd889752a6410709299bec0d7c029
Tobias Jonas <t.jonas@innfactory.de>
2019-03-26 10:15:05 +0100

    init sample app

---
+++ precache-manifest.ca1c56115359a4743dcabdc975e6409c.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "4643c290883a867e4f99",
+    "url": "/static/js/main.8de57393.chunk.js"
+  },
+  {
+    "revision": "11659628d802c4971b1b",
+    "url": "/static/js/2.b2e2f295.chunk.js"
+  },
+  {
+    "revision": "8ae6d0a0f7131f130bf65bf723a529ec",
+    "url": "/index.html"
+  }
+];
